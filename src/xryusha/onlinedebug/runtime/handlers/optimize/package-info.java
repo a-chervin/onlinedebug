934953f45f992974446131d5f155edb82eb7e72c
a-chervin <alexander.chervin@gmail.com>
2017-05-15 13:14:37 +0300

    package info data

---
+++ src/xryusha/onlinedebug/runtime/handlers/optimize/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Optional optimizations package, currently unshared,
+ * lightweight noop {@link xryusha.onlinedebug.runtime.handlers.optimize.OptimizerBase}
+ * is registered instead
+ */
+package xryusha.onlinedebug.runtime.handlers.optimize;
